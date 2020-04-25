2171b21f5e2a3a6056d4e869c62cbb97bacd3a0e
Stephen Tse <stse200@gmail.com>
2020-04-25 00:19:28 -0400

    Home player select UI improvement

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,27 @@
 @extends("layouts.template")
 
 
+@section("javascript")
+
+<script>
+
+$(".player_number_option").on("click", function(){
+  $(".player_number_option").removeClass("selected");
+  $(this).addClass("selected");
+
+  $("#player_number").val($(this).html());
+});
+
+$("#submit_form").on("click", function(){
+  if($.trim($("#player_number").val()) != ""){
+    $("#login_form").submit();
+  }
+});
+
+</script>
+
+@endsection
+
 @section("css")
 <style>
 
@@ -20,12 +41,39 @@
   justify-items: center;
 }
 
-#submit{
+#submit_form{
   margin-top: 25px;
   width: 15%;
   background-color: #22ab24;
   font-size: 14pt;
   cursor: pointer;
+}
+
+.player_number_select{
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+    grid-gap: 5px;
+}
+
+.player_number_option{
+  background-color: #235191;
+  padding: 10px;
+  color: #ffffff;
+  cursor: pointer;
+  font-size: 20pt;
+}
+
+.player_number_option:hover{
+  background-color: #8b33a3;
+}
+
+.selected{
+  background-color: #8b33a3;
+}
+
+.game_key{
+  outline: none;
+  font-size: 18pt;
 }
 
 </style>
@@ -37,13 +85,28 @@
   Welcome to Dai Di!
 </div>
 
-<form method="POST" action={{action("GameController@game")}}>
+<form id="login_form" method="POST" action={{action("GameController@game")}}>
   @csrf
 
   <div class="container">
-    <span>Enter Game Key: <input type="password" name="game_key" required></span>
-    <span>Enter Player Number: <input type="number" name="player_number" required></span>
-    <input id="submit" type="submit" value="Enter Game">
+    <span class="game_key">Enter Game Key: <input class="game_key" type="password" name="game_key" required></span>
+    <input id="player_number" type="hidden" name="player_number">
+    Select Player Number:
+    <div class="player_number_select">
+      <div class="player_number_option">
+        1
+      </div>
+      <div class="player_number_option">
+        2
+      </div>
+      <div class="player_number_option">
+        3
+      </div>
+      <div class="player_number_option">
+        4
+      </div>
+    </div>
+    <button type="button" id="submit_form">Enter Game</button>
   </div>
 </form>
 
