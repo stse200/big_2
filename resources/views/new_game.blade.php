a279dc4d92f9d93c249d4c22a5235978a2f19e69
Stephen Tse <stse200@gmail.com>
2020-05-04 15:48:50 -0400

    Create game form validation

--- resources/views/new_game.blade.php
+++ resources/views/new_game.blade.php
@@ -46,6 +46,16 @@ label:after{
   display: none;
 }
 
+.error{
+  grid-column-start: 1;
+  grid-column-end: 3;
+  color: rgb(218,56,73);
+  text-align: center;
+  font-family: "Arial";
+  font-size: 14pt;
+  display: none;
+}
+
 </style>
 
 @endsection
@@ -54,7 +64,12 @@ label:after{
 
 <script>
 
+$("#game_name").keyup(function(){
+  $("#name_error").css("display", "none");
+});
+
   $(".player").keyup(function(){
+    $("#player_error").css("display", "none");
 
     var player = $(this).attr("id");
 
@@ -92,7 +107,35 @@ label:after{
   });
 
   $("#submit_form").on("click", function(){
-    $("#new_game_form").submit();
+    if($("#game_name").val() == ""){
+      //ASSERT: no game name
+      $("#name_error").css("display", "inline");
+    }
+    else if($("#p1_val").val() == ""){
+      //ASSERT: no p1 entered
+      $("#player_error").css("display", "inline");
+      $("#p_num_error").html(1);
+    }
+    else if($("#p2_val").val() == ""){
+      //ASSERT: no p1 entered
+      $("#player_error").css("display", "inline");
+      $("#p_num_error").html(2);
+    }
+    else if($("#p3_val").val() == ""){
+      //ASSERT: no p1 entered
+      $("#player_error").css("display", "inline");
+      $("#p_num_error").html(3);
+    }
+    else if($("#p4_val").val() == ""){
+      //ASSERT: no p1 entered
+      $("#player_error").css("display", "inline");
+      $("#p_num_error").html(4);
+    }
+    else{
+      //ASSERT: form is complete
+      $("#new_game_form").submit();
+    }
+
   });
 
 </script>
@@ -104,31 +147,39 @@ label:after{
 <form id="new_game_form" method="POST" action="{{action("Big2Controller@create_new_game")}}">
   @csrf
   <div class="container">
-      <div class="input_item_wide"><label>Game Name:</label><input type="text" name="game_name" autocomplete="off" required></div>
+      <div class="input_item_wide"><label>Game Name:</label><input id="game_name" type="text" name="game_name" autocomplete="off" maxlength="25"></div>
 
       <div>
         <label>Player 1: <i id="p1_good" class="fas fa-check"></i><i id="p1_bad" class="fas fa-times"></i></label>
-        <input class="player" type="text" id="p1" required>
+        <input class="player" type="text" id="p1" autocomplete="off">
         <input id="p1_val" type="hidden" name="p1">
       </div>
       <div>
         <label>Player 2: <i id="p2_good" class="fas fa-check"></i><i id="p2_bad" class="fas fa-times"></i></label>
-        <input class="player" type="text" id="p2" required>
+        <input class="player" type="text" id="p2" autocomplete="off">
         <input id="p2_val" type="hidden" name="p2">
       </div>
       <div>
         <label>Player 3: <i id="p3_good" class="fas fa-check"></i><i id="p3_bad" class="fas fa-times"></i></label>
-        <input class="player" type="text" id="p3" required>
+        <input class="player" type="text" id="p3" autocomplete="off">
         <input id="p3_val" type="hidden" name="p3">
       </div>
       <div>
         <label>Player 4: <i id="p4_good" class="fas fa-check"></i><i id="p4_bad" class="fas fa-times"></i></label>
-        <input class="player" type="text" id="p4" required>
+        <input class="player" type="text" id="p4" autocomplete="off">
         <input id="p4_val" type="hidden" name="p4">
       </div>
 
 
     <a id="submit_form"><i class="far fa-sticky-note"></i> Create Game</a>
+
+    <div id="name_error" class="error">
+      Please enter a game name
+    </div>
+
+    <div id="player_error" class="error">
+      Enter a valid username for player <span id="p_num_error">
+    </div>
 
   </div>
 </form>
